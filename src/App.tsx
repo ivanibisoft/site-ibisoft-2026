c418ceeeadfe9618d40623a41263fe80c697279a
ivanibisoft <290226310+ivanibisoft@users.noreply.github.com>
2026-07-31 00:53:03 -0300

    Fix homepage rendering issue

--- src/App.tsx
+++ src/App.tsx
@@ -13,36 +13,39 @@ import Cases from './pages/Cases'
 import NotFound from './pages/NotFound'
 import { ScrollToTop } from './components/ScrollToTop'
 import { SiteAssetsProvider } from '@/hooks/use-site-assets'
+import { AuthProvider } from '@/hooks/use-auth'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import { EditorMiddleware } from './components/EditorMiddleware'
 
 const App = () => (
   <ErrorBoundary>
-    <SiteAssetsProvider>
-      <EditorMiddleware>
-        <BrowserRouter future={{ v7_startTransition: false, v7_relativeSplatPath: false }}>
-          <ScrollToTop />
-          <TooltipProvider>
-            <Toaster />
-            <Sonner />
-            <Routes>
-              <Route element={<Layout />}>
-                <Route path="/" element={<Index />} />
-                <Route path="/segmentos/:slug" element={<Segment />} />
-                <Route path="/funcionalidades/:slug" element={<Functionality />} />
-                <Route path="/quero-conhecer" element={<QueroConhecer />} />
-                <Route path="/sobre-erp" element={<SobreErp />} />
-                <Route path="/sobre" element={<About />} />
-                <Route path="/cases" element={<Cases />} />
-                <Route path="/contato" element={<QueroConhecer />} />
-              </Route>
+    <AuthProvider>
+      <SiteAssetsProvider>
+        <EditorMiddleware>
+          <BrowserRouter future={{ v7_startTransition: false, v7_relativeSplatPath: false }}>
+            <ScrollToTop />
+            <TooltipProvider>
+              <Toaster />
+              <Sonner />
+              <Routes>
+                <Route element={<Layout />}>
+                  <Route path="/" element={<Index />} />
+                  <Route path="/segmentos/:slug" element={<Segment />} />
+                  <Route path="/funcionalidades/:slug" element={<Functionality />} />
+                  <Route path="/quero-conhecer" element={<QueroConhecer />} />
+                  <Route path="/sobre-erp" element={<SobreErp />} />
+                  <Route path="/sobre" element={<About />} />
+                  <Route path="/cases" element={<Cases />} />
+                  <Route path="/contato" element={<QueroConhecer />} />
+                </Route>
 
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </TooltipProvider>
-        </BrowserRouter>
-      </EditorMiddleware>
-    </SiteAssetsProvider>
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </TooltipProvider>
+          </BrowserRouter>
+        </EditorMiddleware>
+      </SiteAssetsProvider>
+    </AuthProvider>
   </ErrorBoundary>
 )
 
